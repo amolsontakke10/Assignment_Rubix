66c2323559472dab607efc2689ced014390d673c
amolsontakke10 <155212385+amolsontakke10@users.noreply.github.com>
2024-12-13 15:18:36 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "semantic-ui-css/semantic.min.css";
 import store from "./store";
 import { Provider } from "react-redux";
 import App from "./App";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
