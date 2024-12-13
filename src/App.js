917388d4e9013b764c25e7d00f9ea4e30892c553
amolsontakke10 <155212385+amolsontakke10@users.noreply.github.com>
2024-12-13 15:14:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,3 @@
-// import React from "react";
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import StreamList from "./components/StreamList";
-// import CreateStream from "./components/CreateStream";
-
-// const App = () => {
-//   return (
-//     <BrowserRouter>
-//       <div className="ui container">
-//         <Routes>
-//           <Route path="/" element={<StreamList />} />
-//           <Route path="/create" element={<CreateStream />} />
-//         </Routes>
-//       </div>
-//     </BrowserRouter>
-//   );
-// };
-
-// export default App;
-
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import StreamList from "./components/StreamList";
